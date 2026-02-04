ff241951bf0e186b15201f618c59088ea445a8fa
Prasad2390 <vanacharlavaraprasad@gmail.com>
2026-02-04 18:45:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,35 @@ variable "volume" {
 
 
 
+------creating multiple resources---
+resource "aws_instance" "myec2" {
+  tags = {
+    Name = var.iname[count.index]
+    ENv  = "prod"
+  }
+  ami                    = "ami-0532be01f26a3de55"
+  instance_type          = var.itype ? "t3.micro" : "t3.small"
+  key_name               = "personal"
+  vpc_security_group_ids = [aws_security_group.mysg.id]
+  root_block_device {
+    volume_size = var.volume
+  }
+  count = 2
+}
+
+variable "iname" {
+  type    = list(string)
+  default = ["myfirst", "second"]
+}
+variable "itype" {
+  type    = bool
+  default = true
+}
+variable "volume" {
+  type    = number
+  default = 20
+}
+
 terraform init
 terraform plan
 terraform apply --auto-approve
